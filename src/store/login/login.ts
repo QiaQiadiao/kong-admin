c7a575d79b642a494318c250ea7f57c71b7c4caa
QiaQiadiao <1832411836@qq.com>
2025-04-01 16:57:23 +0800

    本地缓存和缓存工具的封装

--- src/store/login/login.ts
+++ src/store/login/login.ts
@@ -1,17 +1,20 @@
 import { defineStore } from 'pinia'
 import { accountLoginRequest } from '@/service/api/login/login'
 import type { IAccount } from '@/types/login_types'
+import { localCache } from '@/utils/cache'
+const LOGIN_TOKEN = 'token'
 export const useAccountLoginStore = defineStore('login', () => {
   const data = {
     id: -1,
     name: '',
-    token: '',
+    token: localCache.getCache(LOGIN_TOKEN) ?? '',
   }
   const accountLogin = async (account: IAccount) => {
     const res = await accountLoginRequest(account)
     data.id = res.data.data.id
     data.name = res.data.data.name
     data.token = res.data.data.token
+    localCache.setCache(LOGIN_TOKEN, data.token)
     console.log(res)
     console.log(data)
   }
